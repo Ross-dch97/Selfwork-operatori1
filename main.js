edd1ff08a7b8aa7bb7465d4f5cfcfd2a0881dccf
Ross-dch97 <dchross97@outlook.it>
2024-10-26 01:01:00 +0200

    Selfwork-operatori-1

---
+++ main.js
@@ -0,0 +1,13 @@
+let a = 5;
+let b = 16;
+let c = 23;
+let d = 30;
+let e = 8;
+
+let somma = a + b + c + d + e;
+
+console.log( 'la somma tra i numeri equivale a...', somma );
+
+let media = somma / 5 ;
+
+console.log( 'la media equivale a...', media );
